60abd5150c949fe59eb486a0f631cf772c4f66cc
chanwoo7 <chanwoo9987@gmail.com>
2024-07-29 16:15:19 +0900

    [fix] ModelMapper 사용 시 NoSuchMethodException 오류 해결

--- src/main/java/com/book/backend/domain/auth/dto/SignupDto.java
+++ src/main/java/com/book/backend/domain/auth/dto/SignupDto.java
@@ -5,12 +5,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Getter
 @Setter
+@NoArgsConstructor
 public class SignupDto {
     @NotBlank(message = "아이디는 필수 입력값입니다.")
     private String loginId;
--- src/main/java/com/book/backend/domain/user/dto/UserDto.java
+++ src/main/java/com/book/backend/domain/user/dto/UserDto.java
@@ -1,8 +1,6 @@
 package com.book.backend.domain.user.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -10,6 +8,8 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
     private Long userId;
 
