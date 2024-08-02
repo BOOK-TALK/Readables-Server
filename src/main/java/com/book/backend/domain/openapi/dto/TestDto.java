a2400be77b2b2dbbdf55a0778df75148f4494d87
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-08-03 01:13:55 +0900

    [delete] 시행착오 파일 삭제

--- src/main/java/com/book/backend/domain/openapi/dto/TestDto.java
+++ src/main/java/com/book/backend/domain/openapi/dto/TestDto.java
@@ -1,12 +1,12 @@
-package com.book.backend.domain.openapi.dto;
-
-import jakarta.validation.constraints.NotBlank;
-import lombok.Builder;
-import lombok.Getter;
-
-@Getter
-@Builder
-public class TestDto implements OpenAPIDtoInterface {
-    @NotBlank(message = "pageSize 필수 입력값입니다.")
-    private int pageSize;
-}
+//package com.book.backend.domain.openapi.dto;
+//
+//import jakarta.validation.constraints.NotBlank;
+//import lombok.Builder;
+//import lombok.Getter;
+//
+//@Getter
+//@Builder
+//public class TestDto implements OpenAPIDtoInterface {
+//    @NotBlank(message = "pageSize 필수 입력값입니다.")
+//    private int pageSize;
+//}
