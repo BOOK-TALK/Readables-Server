a01ea14924b04b06b8d68e4f159a41e4d10b7ffb
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-08-03 18:20:11 +0900

    [chore] user 테이블 gender 로 변경

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into user (login_id, password, sex, birth_date, email, phone, reg_date)
+insert into user (login_id, password, gender, birth_date, email, phone, reg_date)
 values ('user1', 'useruser1!', '1', '1990-01-01', 'user1@gmail.com', '010-1111-1111', '2024-06-10'),
        ('user2', 'useruser2!', '2', '2000-03-16', 'user2@gmail.com', '010-2222-2222', '2024-07-22');
