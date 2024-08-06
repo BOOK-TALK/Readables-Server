2e79d08faa1821fb03e5cf691c9ba6cdc2c38fe4
chanwoo7 <chanwoo9987@gmail.com>
2024-08-07 00:31:29 +0900

    [feat] random API용 DTO 추가

---
+++ src/main/java/com/book/backend/domain/openapi/dto/request/genre/RandomRequestDto.java
@@ -0,0 +1,16 @@
+package com.book.backend.domain.openapi.dto.request.genre;
+
+import com.book.backend.domain.openapi.dto.request.OpenAPIRequestInterface;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class RandomRequestDto implements OpenAPIRequestInterface {
+    @NotBlank(message = "subKdc는 필수 입력값입니다.")
+    private String dtl_kdc;
+    private Integer pageSize;  // 페이지 사이즈
+}
