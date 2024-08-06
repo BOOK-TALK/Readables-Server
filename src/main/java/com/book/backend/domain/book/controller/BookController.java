7bf43fd20aa572f44b364c8cccdd33793a8affb0
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-08-06 17:24:52 +0900

    [chore] hotTrend API req Param 삭제

--- src/main/java/com/book/backend/domain/book/controller/BookController.java
+++ src/main/java/com/book/backend/domain/book/controller/BookController.java
@@ -12,11 +12,11 @@ import com.book.backend.domain.openapi.dto.response.RecommendResponseDto;
 import com.book.backend.domain.openapi.dto.request.RecommendRequestDto;
 import com.book.backend.global.ResponseTemplate;
 import com.book.backend.global.log.RequestLogger;
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.LinkedList;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,7 +35,7 @@ public class BookController {
 
     // 마니아(4), 다독자(5) 추천 API
     @GetMapping("/recommend")
-    public ResponseEntity<?> recommend(@RequestParam(required = true) String isbn) throws Exception {
+    public ResponseEntity<?> recommend(@RequestParam String isbn) throws Exception {
         RequestLogger.param(new String[]{"isbn"}, isbn);
         requestValidate.isValidIsbn(isbn);
 
@@ -51,11 +51,9 @@ public class BookController {
 
     // 대출급상승(12) API
     @GetMapping("/hotTrend")
-    public ResponseEntity<?> hotTrend(@RequestParam(required = true) String searchDt) throws Exception {
-        RequestLogger.param(new String[]{"searchDt"}, searchDt);
-        requestValidate.isValidSearchDt(searchDt);
-
-        HotTrendRequestDto requestDto = HotTrendRequestDto.builder().searchDt(searchDt).build();
+    public ResponseEntity<?> hotTrend() throws Exception {
+        LocalDate yesterday = LocalDate.now().minusDays(1);
+        HotTrendRequestDto requestDto = HotTrendRequestDto.builder().searchDt(yesterday.toString()).build();
         LinkedList<HotTrendResponseDto> response = bookService.hotTrend(requestDto);
 
         return responseTemplate.success(response, HttpStatus.OK);
--- src/main/java/com/book/backend/domain/book/service/RequestValidate.java
+++ src/main/java/com/book/backend/domain/book/service/RequestValidate.java
@@ -15,17 +15,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class RequestValidate {
 
-    public void isValidSearchDt(String searchDt) {
-        //yyyy-mm-dd 형식인지 확인
-        if(!searchDt.matches("^\\d{4}-\\d{2}-\\d{2}$")) {
-            throw new CustomException(ErrorCode.INVALID_SEARCH_DT_FORMAT);
-        }
-        // searchDt 가 오늘 이후 날짜라면 에러 처리 (어제까지만 검색 가능)
-        if(LocalDate.parse(searchDt).isAfter(LocalDate.now().minusDays(1))) {
-            throw new CustomException(ErrorCode.INVALID_SEARCH_DT_DATE);
-        }
-    }
-
     public void isValidIsbn(String isbn) {
         // 숫자로 구성된 13자리 ISBN
         if(!(isbn.matches("^[0-9]*$") && isbn.length() == 13)) {
@@ -123,4 +112,12 @@ public class RequestValidate {
             throw new CustomException(ErrorCode.INVALID_LIB_CODE);
         }
     }
+
+
+//    public void isValidSearchDt(String searchDt) {
+//        //yyyy-mm-dd 형식인지 확인
+//        if(!searchDt.matches("^\\d{4}-\\d{2}-\\d{2}$")) {
+//            throw new CustomException(ErrorCode.INVALID_SEARCH_DT_FORMAT);
+//        }
+//    }
 }
