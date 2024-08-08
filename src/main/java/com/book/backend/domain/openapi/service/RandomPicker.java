780024eaaa22c520d21c149291965216c48de0de
chanwoo7 <chanwoo9987@gmail.com>
2024-08-09 00:32:39 +0900

    [chore] RandomPicker 예외처리 수정

--- src/main/java/com/book/backend/domain/openapi/service/RandomPicker.java
+++ src/main/java/com/book/backend/domain/openapi/service/RandomPicker.java
@@ -2,20 +2,20 @@ package com.book.backend.domain.openapi.service;
 
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.Objects;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class RandomPicker {
 
     // list 에서 무작위로 nums 갯수만큼 추출한 리스트를 반환
-    public static <T> LinkedList<T> randomPick(LinkedList<T> list, int nums){
+    public static <T> LinkedList<T> randomPick(LinkedList<T> list, Integer nums){
         log.trace("RandomPicker > randomPick()");
 
-        if (nums > list.size()) {
-            throw new IllegalArgumentException("nums는 list의 크기를 넘을 수 없습니다.");
-        }
+        int maxSize = Objects.requireNonNullElse(nums, 200);
         Collections.shuffle(list);
 
-        return new LinkedList<>(list.subList(0, 10));
+        return new LinkedList<>(list.subList(0, maxSize));
     }
 }
