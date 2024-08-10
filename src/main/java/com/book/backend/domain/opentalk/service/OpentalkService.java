0bb1626b1fa0dbaff7c2590221abb2d1dc3b58cc
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-08-10 17:34:07 +0900

    [fix] #46 userId 로 해당 유저와 연결된 opentalk id list 반환

--- src/main/java/com/book/backend/domain/opentalk/service/OpentalkService.java
+++ src/main/java/com/book/backend/domain/opentalk/service/OpentalkService.java
@@ -2,10 +2,13 @@ package com.book.backend.domain.opentalk.service;
 
 import com.book.backend.domain.user.entity.User;
 import com.book.backend.domain.user.repository.UserRepository;
+import com.book.backend.domain.userOpentalk.entity.UserOpentalk;
+import com.book.backend.domain.userOpentalk.repository.UserOpentalkRepository;
 import com.book.backend.exception.CustomException;
 import com.book.backend.exception.ErrorCode;
+
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -13,10 +16,17 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class OpentalkService {
     private final UserRepository userRepository;
+    private final UserOpentalkRepository userOpentalkRepository;
 
     /* 해당 user의 즐찾 opentalk list 반환*/
     public List<?> main(String loginId) {
         User user =  userRepository.findByLoginId(loginId).orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
-        return user.getOpenTalkIds();
+        List<UserOpentalk> opentalkList = userOpentalkRepository.findAllByUserId(user);
+
+        List<Long> opentalkIds = new LinkedList<>();
+        for(UserOpentalk userOpentalk : opentalkList) {
+            opentalkIds.add(userOpentalk.getOpentalkId().getOpentalkId());
+        }
+        return opentalkIds;
     }
 }
