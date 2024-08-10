ddbc2b44020c5d374349162b4008c63640fe543b
chanwoo7 <chanwoo9987@gmail.com>
2024-08-11 03:31:56 +0900

    [feat] 액세스 토큰 기반 유저 정보 요청 로직 작성

--- src/main/java/com/book/backend/domain/auth/service/KakaoService.java
+++ src/main/java/com/book/backend/domain/auth/service/KakaoService.java
@@ -1,9 +1,11 @@
 package com.book.backend.domain.auth.service;
 
 import com.book.backend.domain.auth.dto.KakaoTokenResponseDto;
+import com.book.backend.domain.auth.dto.KakaoUserInfoDto;
 import com.book.backend.exception.CustomException;
 import com.book.backend.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -18,6 +20,7 @@ import static org.springframework.security.oauth2.core.endpoint.OAuth2ParameterN
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class KakaoService {
     @Value("${kakao.restapiKey}")
     private String restApiKey;
@@ -25,11 +28,15 @@ public class KakaoService {
     private String redirectUri;
     @Value("${kakao.tokenRequestUri}")
     private String tokenRequestUri;
+    @Value("${kakao.userInfoUri}")
+    private String userInfoUri;
 
     private final RestTemplate restTemplate;
 
     // Redirect URI에 전달된 코드값으로 Access Token 요청
     public KakaoTokenResponseDto getAccessToken(String authorizationCode) {
+        log.trace("getAccessToken");
+
         try {
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
@@ -43,6 +50,29 @@ public class KakaoService {
             HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, headers);
 
             return restTemplate.postForEntity(tokenRequestUri, request, KakaoTokenResponseDto.class).getBody();
+        } catch (HttpClientErrorException e) {
+            throw new CustomException(ErrorCode.BAD_REQUEST);
+        } catch (HttpServerErrorException e) {
+            throw new CustomException(ErrorCode.KAKAO_SERVER_ERROR);
+        }
+    }
+
+    // Access Token으로 유저 정보 요청
+    public KakaoUserInfoDto getUserInfo(String accessToken) {
+        log.trace("getUserInfo");
+
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+            headers.setBearerAuth(accessToken);
+
+            HttpEntity<String> request = new HttpEntity<>(headers);
+
+            // TODO: 일단 id만 받아오도록 구현, 향후 필요 시 유저 프로필 정보 가져오도록 구현
+            ResponseEntity<KakaoUserInfoDto> response = restTemplate.exchange(
+                    userInfoUri, HttpMethod.GET, request, KakaoUserInfoDto.class);
+            return response.getBody();
+
         } catch (HttpClientErrorException e) {
             throw new CustomException(ErrorCode.BAD_REQUEST);
         } catch (HttpServerErrorException e) {
