61163c8dc718186bad0731c41b10992e8ef46918
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-08-10 17:25:34 +0900

    [fix] #46 SQL script에 user_opentalk_id 추가 (INSERT IGNORE)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -16,12 +16,12 @@ INSERT IGNORE INTO user (user_id, login_id, password, gender, birth_date, email,
 values (1, 'user1', 'useruser1!', '1', '1990-01-01', 'user1@gmail.com', '010-1111-1111', '2024-06-10'),
        (2, 'user2', 'useruser2!', '2', '2000-03-16', 'user2@gmail.com', '010-2222-2222', '2024-07-22');
 
-INSERT IGNORE INTO user_opentalk(user_id, opentalk_id)
-values (1, 1),
-       (1, 2),
-       (2, 2),
-       (2, 4),
-       (2, 5);
+INSERT IGNORE INTO user_opentalk(user_opentalk_id, user_id, opentalk_id)
+values (1, 1, 1),
+       (2, 1, 2),
+       (3, 2, 2),
+       (4, 2, 4),
+       (5, 2, 5);
 
 -- 장르 초기화 (한국십진분류법(KDC) 6판 기준)
 INSERT IGNORE INTO GENRE (parent_genre_id, kdc_num, name)
