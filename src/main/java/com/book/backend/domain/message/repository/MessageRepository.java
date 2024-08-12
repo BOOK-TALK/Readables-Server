6e29727ff2cbfef51b1b84ab192873f13153967e
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-08-13 00:07:39 +0900

    [feat] #50 message 테이블 opentalkId 칼럼 기준으로 페이지네이션

--- src/main/java/com/book/backend/domain/message/repository/MessageRepository.java
+++ src/main/java/com/book/backend/domain/message/repository/MessageRepository.java
@@ -2,19 +2,17 @@ package com.book.backend.domain.message.repository;
 
 import com.book.backend.domain.message.entity.Message;
 import com.book.backend.domain.opentalk.entity.Opentalk;
-import com.book.backend.domain.user.entity.User;
-import com.book.backend.domain.userOpentalk.entity.UserOpentalk;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 @Repository
 public interface MessageRepository extends JpaRepository<Message, Long> {
     List<Message> findTop200ByOrderByCreatedAtDesc();
 
-    // 메세지 페이지네이션
-//    Page<Message> findAll(Pageable pageable);
+    // 특정 opentalk Id 를 갖는 데이터 조회 (페이지네이션)
+    Page<Message> findAllByOpentalk(Opentalk opentalk, Pageable pageable);
 }
