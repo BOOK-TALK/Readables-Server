14157a9d96b24341bd6a9939250302e8c4c7bc41
chanwoo7 <chanwoo9987@gmail.com>
2024-08-13 21:15:46 +0900

    [chore] securityfilterChain 수정

--- src/main/java/com/book/backend/global/SecurityConfig.java
+++ src/main/java/com/book/backend/global/SecurityConfig.java
@@ -4,11 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -24,13 +26,17 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
         http
+                .cors(Customizer.withDefaults())  // CORS 활성화
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) -> authorize
-//                        .requestMatchers("/api/**", "/v3/api-docs/**", "/swagger-ui/**").permitAll()
-                        .requestMatchers("/**").permitAll()
+                        .requestMatchers("/api/**", "/v3/api-docs/**", "/swagger-ui/**").permitAll()  // Swagger 관련 경로
+                        .requestMatchers("/login/oauth2/**").permitAll()  // OAuth2 Callback 경로
+                        .requestMatchers("/token/**").permitAll()  // 토큰 발급 경로
                         .anyRequest().authenticated()
+                )
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)  // 세션 관리 비활성화
                 )
                 .formLogin(AbstractHttpConfigurer::disable)
                 .logout(AbstractHttpConfigurer::disable);
