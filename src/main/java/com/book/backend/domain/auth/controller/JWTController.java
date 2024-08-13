449ef150e7e3a8087350f52fd6b7e8dcd5389ced
chanwoo7 <chanwoo9987@gmail.com>
2024-08-13 23:24:23 +0900

    [feat] Refresh Token을 함께 관리하도록 수정

--- src/main/java/com/book/backend/domain/auth/controller/JWTController.java
+++ src/main/java/com/book/backend/domain/auth/controller/JWTController.java
@@ -1,49 +1,49 @@
 package com.book.backend.domain.auth.controller;
 
+import com.book.backend.domain.auth.dto.JwtTokenDto;
 import com.book.backend.domain.auth.dto.LoginDto;
 import com.book.backend.domain.auth.dto.LoginSuccessResponse;
 import com.book.backend.domain.auth.service.CustomUserDetailsService;
-import com.book.backend.domain.user.entity.User;
-import com.book.backend.domain.user.repository.UserRepository;
+import com.book.backend.global.ResponseTemplate;
 import com.book.backend.util.JwtUtil;
-import jakarta.annotation.PostConstruct;
+import io.swagger.v3.oas.models.links.Link;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.LinkedList;
+
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-public class JWTController {
+public class JwtController {
     private final CustomUserDetailsService userDetailsService;
     private final JwtUtil jwtUtil;
     private final AuthenticationManager authenticationManager; // authenticate 메서드 : username, password 기반 인증 수행
+    private final ResponseTemplate responseTemplate;
 
     @PostMapping("/api/auth/jwt")
-    public ResponseEntity<LoginSuccessResponse> authenticateTest(@RequestBody LoginDto dto) {
+    public ResponseEntity<?> authenticateTest(@RequestBody LoginDto dto) {
         log.info("/auth 호출");
         try {
             // 인증 시도
-            authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(dto.getLoginId(), dto.getPassword()));
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(dto.getLoginId(), dto.getPassword()));
         } catch (BadCredentialsException e) {
             throw new BadCredentialsException("로그인 실패");
         }
-        // 인증 성공 후 인증된 user의 정보를 갖고옴
+        // 인증 성공 후 인증된 user의 정보를 갖고 옴
         UserDetails userDetails = userDetailsService.loadUserByUsername(dto.getLoginId());
         // subject, claim 모두 UserDetails를 사용하므로 객체를 그대로 전달
-        String token = jwtUtil.generateToken(userDetails);
+        JwtTokenDto jwtTokenDto = jwtUtil.generateToken(userDetails);
 
-        // 생성된 토큰을 응답 (Test)
-        return ResponseEntity.ok(new LoginSuccessResponse(token));
+        return responseTemplate.success(jwtTokenDto, HttpStatus.OK);
     }
 }
