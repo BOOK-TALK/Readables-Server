716749230cb708a6c557c2a36ab7f227068ccd16
chanwoo7 <chanwoo9987@gmail.com>
2024-08-14 08:03:59 +0900

    [feat] RequestWrapper 추가 및 JSON 파싱 오류 해결

---
+++ src/main/java/com/book/backend/util/RequestWrapper.java
@@ -0,0 +1,36 @@
+package com.book.backend.util;
+
+import jakarta.servlet.ReadListener;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequestWrapper;
+
+import java.io.*;
+
+public class RequestWrapper extends HttpServletRequestWrapper {
+    /**
+     * Constructs a request object wrapping the given request.
+     *
+     * @param request The request to wrap
+     * @throws IllegalArgumentException if the request is null
+     */
+    private byte[] cachedBody;
+
+    public RequestWrapper(HttpServletRequest request) throws IOException {
+        super(request);
+        InputStream requestInputStream = request.getInputStream();
+        this.cachedBody = requestInputStream.readAllBytes();
+    }
+
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        return new CachedBodyServletInputStream(this.cachedBody);
+    }
+
+    @Override
+    public BufferedReader getReader() throws IOException {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(this.cachedBody);
+        return new BufferedReader(new InputStreamReader(byteArrayInputStream));
+    }
+
+}
