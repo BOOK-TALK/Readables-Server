f848cf15037fb41c299d83ddcabe4f8d56c63c38
chanwoo7 <chanwoo9987@gmail.com>
2024-08-17 12:19:56 +0900

    [refactor] JWT 인증 에러 메시지 세분화

--- src/main/java/com/book/backend/global/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/book/backend/global/CustomAuthenticationEntryPoint.java
@@ -1,7 +1,6 @@
 package com.book.backend.global;
 
 import com.book.backend.exception.CustomException;
-import com.book.backend.exception.ErrorCode;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -13,7 +12,7 @@ import java.io.IOException;
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        CustomException customException = new CustomException(ErrorCode.LOGIN_REQUIRED);
+        CustomException customException = (CustomException) request.getAttribute("JWTException");
         response.setStatus(customException.getCode().getStatus().value());
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
