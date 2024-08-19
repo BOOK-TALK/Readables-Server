e75b7198d49e93d580553b0b3a9f8c94dd79f3ec
chanwoo7 <chanwoo9987@gmail.com>
2024-08-19 16:38:36 +0900

    [chore] DTO에 누락된 필드 추가

--- src/main/java/com/book/backend/domain/user/dto/UserDto.java
+++ src/main/java/com/book/backend/domain/user/dto/UserDto.java
@@ -12,28 +12,22 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class UserDto {
     private Long userId;
-
-    private String kakaoId;
-
-    private LocalDateTime regDate;
-
     private String loginId;
-
+    private String kakaoId;
+    private LocalDateTime regDate;
+    private String nickname;
     private String password;
-
     private String gender;
-
     private LocalDate birthDate;
-
     private String email;
-
     private String phone;
 
     public static final String description =
             "userId : 유저 아이디 | " +
+            "loginId : 유저 로그인 아이디 (일반 로그인 시 생성) | " +
             "kakaoId : 유저 카카오 아이디 (카카오 로그인 시 생성) | " +
             "regDate : 가입 시각 | " +
-            "loginId : 유저 로그인 아이디 (일반 로그인 시 생성) | " +
+            "nickname : 닉네임 | " +
             "password : 암호화된 비밀번호 (일반 로그인 시 생성) | " +
             "gender : 성별 | " +
             "birthDate : 생일 | " +
