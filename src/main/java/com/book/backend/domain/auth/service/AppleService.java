8aee60970142033aec29f71efa0cecaaa681430d
chanwoo7 <chanwoo9987@gmail.com>
2024-08-25 03:05:30 +0900

    [refactor] CustomUserDetailsService 리팩토링

--- src/main/java/com/book/backend/domain/auth/service/AppleService.java
+++ src/main/java/com/book/backend/domain/auth/service/AppleService.java
@@ -58,7 +58,7 @@ public class AppleService {
         userRepository.save(user);
 
         // UserDetailsService를 사용하여 UserDetails 객체 생성
-        UserDetails userDetails = userDetailsService.loadUserByAppleId(providerId);
+        UserDetails userDetails = userDetailsService.loadUserByUsername(providerId);
         JwtTokenDto jwtTokenDto = jwtUtil.generateToken(userDetails);
 
         // 사용자 인증 정보 생성 및 SecurityContext에 저장
