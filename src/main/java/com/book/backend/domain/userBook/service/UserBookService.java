fbfc111cde67ab677ee5537d52c0f9085a5271a3
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-08-25 03:09:41 +0900

    [feat] #104 읽은책 추가, 삭제 API 및 마이페이지 접속 시 읽은책 list 반환 로직 추가

--- src/main/java/com/book/backend/domain/userBook/service/UserBookService.java
+++ src/main/java/com/book/backend/domain/userBook/service/UserBookService.java
@@ -19,8 +19,48 @@ public class UserBookService {
     private final UserService userService;
     private final UserRepository userRepository;
 
+    //읽은책 추가
+    public List<UserBookDto> setReadBooks(String isbn, String bookname, String bookImgUrl) {
+        log.trace("UserBookService > setReadBooks()");
+        User user = userService.loadLoggedinUser();
+        if (user == null) {
+            throw new CustomException(ErrorCode.USER_NOT_FOUND);
+        }
+        UserBookDto userBookDto = UserBookDto.builder()
+                .isbn(isbn)
+                .bookname(bookname)
+                .bookImageURL(bookImgUrl)
+                .build();
+        user.getReadBooks().add(userBookDto);
+        userRepository.save(user);
+
+        return user.getReadBooks();
+    }
+
+    //읽은책 삭제
+    public List<UserBookDto> removeReadBooks(String isbn) {
+        log.trace("UserBookService > removeReadBooks()");
+        User user = userService.loadLoggedinUser();
+        if (user == null) {
+            throw new CustomException(ErrorCode.USER_NOT_FOUND);
+        }
+        user.getReadBooks().removeIf(userBookDto -> userBookDto.getIsbn().equals(isbn));
+        userRepository.save(user);
+        return user.getReadBooks();
+    }
+
+    //읽은책 조회
+    public List<UserBookDto> getReadBooks() {
+        log.trace("UserBookService > getReadBooks()");
+        User user = userService.loadLoggedinUser();
+        if (user == null) {
+            throw new CustomException(ErrorCode.USER_NOT_FOUND);
+        }
+        return user.getReadBooks();
+    }
+
     // 찜 설정
-    public List<UserBookDto> setDibs(String isbn, String bookname, String bookImgUrl) {
+    public List<UserBookDto> setDibsBooks(String isbn, String bookname, String bookImgUrl) {
         log.trace("UserBookService > setDibs()");
         User user = userService.loadLoggedinUser();
         if (user == null) {
@@ -38,7 +78,7 @@ public class UserBookService {
     }
 
     // 찜 해제
-    public List<UserBookDto> removeDibs(String isbn) {
+    public List<UserBookDto> removeDibsBooks(String isbn) {
         log.trace("UserBookService > removeDibs()");
         User user = userService.loadLoggedinUser();
         if (user == null) {
@@ -50,7 +90,7 @@ public class UserBookService {
     }
 
     // 찜 리스트 조회
-    public List<UserBookDto> getDibsList() {
+    public List<UserBookDto> getDibsBooks() {
         log.trace("UserBookService > getDibsList()");
         User user = userService.loadLoggedinUser();
         if (user == null) {
