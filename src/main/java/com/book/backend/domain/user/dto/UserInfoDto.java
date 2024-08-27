98381c4073e6d03db82ee1202d087a9ae35a6444
chanwoo7 <chanwoo9987@gmail.com>
2024-08-28 02:41:16 +0900

    [fix] 유저 정보 수정 시 일부 필드 제약 해제

--- src/main/java/com/book/backend/domain/user/dto/UserInfoDto.java
+++ src/main/java/com/book/backend/domain/user/dto/UserInfoDto.java
@@ -16,11 +16,8 @@ public class UserInfoDto {
     @NotBlank(message = "닉네임은 필수 입력값입니다.")
     private String nickname;
 
-    @NotBlank(message = "성별은 필수 입력값입니다.")
-    @Pattern(regexp = "NOT_SELECTED|MAN|WOMAN", message = "성별은 NOT_SELECTED, MAN, WOMAN 중 하나여야 합니다.")
     private String gender;
 
-    @Past(message = "현재 날짜보다 이전 날짜여야 합니다.")
     private LocalDate birthDate;
 
     private String profileImageUrl; // TODO : 구현
