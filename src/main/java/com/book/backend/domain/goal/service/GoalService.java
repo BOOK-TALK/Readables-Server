28f38503e0f40647fac956be96cb8c590f4f768d
chanwoo7 <chanwoo9987@gmail.com>
2024-08-28 04:10:42 +0900

    [refactor] #123 GoalRequestValidate 컴포넌트화

--- src/main/java/com/book/backend/domain/goal/service/GoalService.java
+++ src/main/java/com/book/backend/domain/goal/service/GoalService.java
@@ -21,14 +21,14 @@ import java.time.LocalDateTime;
 @Slf4j
 public class GoalService {
     private final GoalRepository goalRepository;
-    private final UserService userService;
+    private final GoalRequestValidate goalRequestValidate;
     private final GoalMapper goalMapper;
 
     public GoalDto getGoal(Long goalId) throws Exception {
         log.trace("GoalService > getGoal()");
 
         // 목표 조회
-        Goal goal = validateAndGetGoal(goalId);
+        Goal goal = goalRequestValidate.validateAndGetGoal(goalId);
 
         return goalMapper.convertToGoalDto(goal);
     }
@@ -37,8 +37,8 @@ public class GoalService {
     public GoalDto createGoal(String isbn, String totalPage) throws Exception {
         log.trace("GoalService > createGoal()");
 
-        User user = validateAndGetLoggedInUser();
-        validateIsExistGoal(user, isbn);
+        User user = goalRequestValidate.validateAndGetLoggedInUser();
+        goalRequestValidate.validateIsExistGoal(user, isbn);
 
         LocalDateTime now = LocalDateTime.now();
 
@@ -62,9 +62,9 @@ public class GoalService {
     public GoalDto finishGoal(Long goalId) throws Exception {
         log.trace("GoalService > finishGoal()");
 
-        User user = validateAndGetLoggedInUser();
-        Goal goal = validateAndGetGoal(goalId);
-        validateUserMatchesGoal(user, goal);
+        User user = goalRequestValidate.validateAndGetLoggedInUser();
+        Goal goal = goalRequestValidate.validateAndGetGoal(goalId);
+        goalRequestValidate.validateUserMatchesGoal(user, goal);
 
         // 목표 완료
         goal.setIsFinished(true);
@@ -77,48 +77,13 @@ public class GoalService {
     public void deleteGoal(Long goalId) {
         log.trace("GoalService > deleteGoal()");
 
-        User user = validateAndGetLoggedInUser();
-        Goal goal = validateAndGetGoal(goalId);
-        validateUserMatchesGoal(user, goal);
+        User user = goalRequestValidate.validateAndGetLoggedInUser();
+        Goal goal = goalRequestValidate.validateAndGetGoal(goalId);
+        goalRequestValidate.validateUserMatchesGoal(user, goal);
 
         // 목표 삭제
         goalRepository.delete(goal);
     }
 
-    // 유저 검증 및 로그인된 유저 로드
-    private User validateAndGetLoggedInUser() {
-        log.trace("GoalService > validateAndGetLoggedInUser()");
 
-        User user = userService.loadLoggedinUser();
-        if (user == null) {
-            throw new CustomException(ErrorCode.USER_NOT_FOUND);
-        }
-        return user;
-    }
-
-    // 목표 검증 및 로드
-    private Goal validateAndGetGoal(Long goalId) {
-        log.trace("GoalService > validateAndGetLoggedInUser()");
-
-        return goalRepository.findById(goalId)
-                .orElseThrow(() -> new CustomException(ErrorCode.GOAL_NOT_FOUND));
-    }
-
-    // 해당 유저의 목표인지 검증
-    private void validateUserMatchesGoal(User user, Goal goal) {
-        log.trace("GoalService > validateUserMatchesGoal()");
-
-        if (user != goal.getUser()) {
-            throw new CustomException(ErrorCode.CANNOT_ACCESS_GOAL);
-        }
-    }
-
-    // 해당 책에 대한 목표가 이미 존재하는지 검증
-    private void validateIsExistGoal(User user, String isbn) {
-        log.trace("GoalService > validateIsExistGoal()");
-
-        if (goalRepository.findByUserAndIsbn(user, isbn).isPresent()) {
-            throw new CustomException(ErrorCode.GOAL_IS_ALREADY_EXIST);
-        }
-    }
 }
