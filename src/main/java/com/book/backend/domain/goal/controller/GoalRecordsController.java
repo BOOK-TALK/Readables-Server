c96f02c505a916f29fc1eb13b23b38eab078abcb
chanwoo7 <chanwoo9987@gmail.com>
2024-08-28 23:29:30 +0900

    [chore] #125 잘못 작성한 애노테이션 수정

--- src/main/java/com/book/backend/domain/goal/controller/GoalRecordsController.java
+++ src/main/java/com/book/backend/domain/goal/controller/GoalRecordsController.java
@@ -9,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/goal/record")
@@ -24,7 +21,7 @@ public class GoalRecordsController {
     private final RequestValidate requestValidate;
     private final ResponseTemplate responseTemplate;
 
-    @GetMapping("/add")
+    @PostMapping("/add")
     public ResponseEntity<?> addRecord(@RequestParam Long goalId,
                                        @RequestParam Integer recentPage) throws Exception {
         log.trace("GoalRecordsController > addRecord()");
