a5bf1d7041bb60454e0380a6750640373895bb22
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-08-29 21:54:06 +0900

    [fix] 서버 시간 서울로 설정

--- src/main/java/com/book/backend/BackendApplication.java
+++ src/main/java/com/book/backend/BackendApplication.java
@@ -1,11 +1,18 @@
 package com.book.backend;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class BackendApplication {
 
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul")); //서버 시간 설정
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
