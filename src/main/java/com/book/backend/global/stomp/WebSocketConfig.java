165773d8b0435ecaae8bf31fba5c2cf69156ac11
hyeesw <hyeeun7904@kookmin.ac.kr>
2024-09-23 22:01:29 +0900

    [feat] #155 webSocketConfig.java 에 heartbeat 수신 코드 추가

--- src/main/java/com/book/backend/global/stomp/WebSocketConfig.java
+++ src/main/java/com/book/backend/global/stomp/WebSocketConfig.java
@@ -30,6 +30,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(final MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/pub"); // 발행요청
-        registry.enableSimpleBroker("/sub"); // 구독요청
+        registry.enableSimpleBroker("/sub")// 구독요청
+                .setHeartbeatValue(new long[]{0, 10000}); // 클라이언트로부터 10초마다 ping 받는것으로 소켓 연결 유지
     }
 }
--- src/main/java/com/book/backend/util/JwtUtil.java
+++ src/main/java/com/book/backend/util/JwtUtil.java
@@ -10,6 +10,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.Authentication;
@@ -18,6 +19,7 @@ import org.springframework.stereotype.Service;
 
 import java.security.PublicKey;
 import java.util.Date;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
